a475a6b8ca36daa9496748692bb4bb1157ff0f54
MrHUANG <19350955242qq.com>
2022-08-12 05:59:40 +0800

    -

---
+++ Unity/Assets/Editor/AssetBundlePackage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+public class AssetBundlePackage
+{
+    [MenuItem("常用工具/打包AssetBundle")]
+    static void BuildAllAssetBundles()
+    {
+        string folder = "AssetBundle";
+        if (!Directory.Exists(folder))
+        {
+            Directory.CreateDirectory(folder);
+        }
+        BuildPipeline.BuildAssetBundles("AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSX);
+    }
+}
---
+++ Unity/Assets/Script/ShowAtlas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+public class ShowAtlas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image img;
+    private float f;
+    void Start()
+    {
+        img = this.transform.GetComponentInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = (int)f%11;
+        SpriteAtlas spriteAtlas = Resources.Load<SpriteAtlas>("atlas");
+        Debug.Log(spriteAtlas == null);
+        img.sprite = spriteAtlas.GetSprite(i.ToString());
+        f = f + Time.deltaTime;
+    }
+}
